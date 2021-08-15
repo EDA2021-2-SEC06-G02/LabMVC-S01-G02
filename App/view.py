39ef092e86bce15bb58d3bc3b79ffd8c6c73ef7d
sbulan <s.bulan@uniandes.edu.co>
2021-08-15 14:55:46 -0500

    Entrega Final - Laboratorio 1

--- App/view.py
+++ App/view.py
@@ -42,7 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TODO: Modificaciones para el laboratorio 1.
+    print("3- Cargar Book Tags")
     print("0- Salir")
 
 
@@ -58,6 +58,10 @@ def loadTags():
     Carga los Tags
     """
     return controller.loadTags('GoodReads/tags.csv')
+
+def loadBooksTags():
+
+    return controller.loadBooksTags('GoodReads/book_tags.csv')
 
 
 """
@@ -76,7 +80,10 @@ while True:
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
 
-    # TODO: Modificaciones para el laboratorio 1.
+    elif int(inputs[0]) == 3:
+        print("Cargando información de tags....")
+        book_tags = loadTags()
+        print('Total de book tags cargados: ' + str(lt.size(book_tags)))
 
     else:
         sys.exit(0)
--- Data/readme.txt
+++ Data/readme.txt
@@ -1,1 +1,2 @@
-En este directorio van los datos de prueba de los proyectos
+Santiago Bula Naranjo s.bulan 201712286
+Natalia Mendivelso Grijalba n.mendivelso 202112093
